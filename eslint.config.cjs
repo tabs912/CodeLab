a60f9188316dcb66953ffd0f1822d556a2eb4af4
tabs912 <tabitharodman@gmail.com>
2026-07-20 16:45:26 +0000

    const js = require("@eslint/js"); const globals = require("globals"); const googleappsscript = require("eslint-plugin-googleappsscript");
    module.exports = [
      {
        files: [
          "Master_List/Current Production Script/v.1.7.7_Current_Production_Script.js",
          "Master_List/Current Production Script/v.1.7.7_Current_Production_Script.gs"
        ],
    
        languageOptions: {
          ecmaVersion: "latest",
          sourceType: "script",
    
          globals: {
            ...globals.es2021,
            ...googleappsscript.environments.googleappsscript.globals
          }
        },
    
        ...js.configs.recommended,
    
        rules: {
          "no-undef": "error",
          "no-redeclare": "error",
    
          "no-unused-vars": [
            "warn",
            {
              vars: "all",
              args: "none",
              varsIgnorePattern:
                "^(onEdit|onOpen|onInstall|doGet|doPost)$|^_.+|.*_$"
            }
          ]
        }
      }
    ];

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,35 +1,3 @@
-const js = require("@eslint/js");
-const globals = require("globals");
-const googleappsscript = require("eslint-plugin-googleappsscript");
-
-module.exports = [
-  {
-    ignores: [
-      "node_modules/**",
-      "Archive/**",
-      "archive/**",
-      "Report/**",
-      "reports/**",
-      "dist/**",
-      "build/**",
-      "coverage/**"
-    ]
-  },
-
-  js.configs.recommended,
-
-  {
-    files: ["**/*.{js,gs}"],
-
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "script",
-
-      globals: {
-        ...globals.es2021,
-        ...googleappsscript.environments.googleappsscript.globals
-      }
-    },
 
     rules: {
       "no-undef": "error",
